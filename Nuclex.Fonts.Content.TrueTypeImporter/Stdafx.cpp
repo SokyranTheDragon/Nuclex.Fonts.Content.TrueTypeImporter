3bfb546bdd7311ae52718cba4d7aed0b2783b2c5
exploder1531 <exploder1531@gmail.com>
2018-02-21 23:20:00 +0100

    Initial commit

--- Nuclex.Fonts.Content.TrueTypeImporter/Stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Nuclex.Fonts.Content.TrueTypeImporter.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
