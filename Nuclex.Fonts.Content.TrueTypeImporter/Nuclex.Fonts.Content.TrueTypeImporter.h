f49f2a2e1e47ec0265bd4c486d8d0159e27f2095
exploder1531 <exploder1531@gmail.com>
2018-02-21 22:08:14 +0100

    Add project files.

---
+++ Nuclex.Fonts.Content.TrueTypeImporter/Nuclex.Fonts.Content.TrueTypeImporter.h
@@ -0,0 +1,13 @@
+// Nuclex.Fonts.Content.TrueTypeImporter.h
+
+#pragma once
+
+using namespace System;
+
+namespace NuclexFontsContentTrueTypeImporter {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ Nuclex.Fonts.Content.TrueTypeImporter/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Nuclex.Fonts.Content.TrueTypeImporter.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
